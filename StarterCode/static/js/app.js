f4b2f723801073f243ddd5f41d7d2e0cbc211e9e
Lauren Stein <lmstein22@gmail.com>
2020-09-28 21:26:57 -0400

    Graph for Patient 1 + early dropdown code

--- StarterCode/static/js/app.js
+++
@@ -1,5 +0,0 @@
-//Fetch and Read JSON file
-d3.json("data/samples.json").then((importedData) => {
-  console.log(importedData);
-  var data = importedData;
-});
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,51 @@
-d3.json("data/samples.json").then((importedData) => {
-  console.log(importedData);
-  var data = importedData;
+d3.json("/data/samples.json").then((importedData) => {
+  //Access sample information
+  var samples = importedData.samples;
+  var sliceValues = samples.map((value) => value.sample_values.slice(0, 10));
+  var sliceOTU = samples.map((otu) => otu.otu_ids.slice(0, 10));
+  // console.log(sliceOTU);
+  console.log(sliceValues[0]);
+  var trace1 = {
+    x: sliceOTU[0],
+    y: sliceValues[0],
+    type: "bar",
+  };
+
+  var data = [trace1];
+
+  Plotly.newPlot("bar", data);
+
+  //=========Make a Graph for initial data =========//
+  //   var makeVis = function (patientID) {
+  //     // Handler for dropdown value change
+  //     var dropdownChange = function () {
+  //       var newPatient = d3.select(this).property("value"),
+  //         newData = names[newPatient];
+
+  //       updateBars(newData);
+
+  //       // Get patient IDs for dropdown
+  //       var patientID = Object.values(importedData.names);
+
+  //       var dropdown = d3
+  //         .select("#selDataset")
+  //         .insert("select", "svg")
+  //         .on("change", dropdownChange);
+
+  //       dropdown
+  //         .selectAll("option")
+  //         .data(patientID)
+  //         .enter()
+  //         .append("option")
+  //         .attr("value", function (d) {
+  //           return d;
+  //         })
+  //         .text("value", function (d) {
+  //           return d;
+  //         });
+
+  //       var initData = samples[0];
+  //     };
+  //     makeVis(initData);
+  //   };
 });
